ec0176d636244d1512f8617b270362d43b6d8a53
Xendexe <92429814+Xendexe@users.noreply.github.com>
2021-11-29 21:51:07 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require("./config.json");
+const ms = require("ms")
+
+
+client.on('ready', () => {
+  console.log(`Bejelentkezve..`);
+  client.user.setPresence({ game: { name: `Készítette: Xendexe`, type: 'WATCHING' }, status: 'dnd' });
+});
+
+
+client.on('message', async (message) => {
+    if (message.author.bot) return;
+    if (message.content.indexOf(config.prefix) !== 0) return;
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+  
+    try {
+      let commandFile = require(`./parancsok/${command}.js`);
+      commandFile.run(client, message, args);
+    } catch (err) {
+   }
+  });
+
+  
+client.login('ODk2ODM1Njc0MTE0NzY0ODUw.YWM5Tw.ag4w3_yYiXSOv1viRn68xWfv2d0');  
+
+
+  
